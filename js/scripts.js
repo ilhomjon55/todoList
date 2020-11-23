ed6bc4b76dbf570fbee4d8d5d2bf50bc86c05292
ilhomjon55 <ilhomshamidinov55@gmail.com>
2020-11-23 07:47:32 +0500

    Review code and add comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,22 @@ EVENT OF ADDING AN ACTION
 ================================ */
 
 
+// Get html elements
 var elActionsListForm = document.querySelector('.actions-list__form');
 var elNewActionInput = elActionsListForm.querySelector('.actions-input');
 var elActionsList = elActionsListForm.querySelector('.actions__list');
 var elActionUrgentInput = elActionsListForm.querySelector('.urgent-checkbox');
 
 
+// Create empty array for action objects
 var actions = [];
 
 
-// Second way =========================
-
+// Listen to submit of elActionsListForm
 elActionsListForm.addEventListener('submit', function (evt) {
    evt.preventDefault();
 
+   // Get value of newActionInput
    var newActionInput = elNewActionInput.value.trim();
 
    // Preventing empty list-item
@@ -35,29 +37,29 @@ elActionsListForm.addEventListener('submit', function (evt) {
 
    // Checking the checkbox and adding to array
    if (elActionUrgentInput.checked) {
-
+      // Create new object and assign its values
       var newObjAction = {
          name: newActionInput,
          isUrgent: true
       }
+      // Unshift to actions array 
       actions.unshift(newObjAction);
-
       elActionsItem.classList.add('alert-urgent');
       elActionsList.prepend(elActionsItem);
 
    } else {
-
+      // Create new object and assign its values
       var newObjAction = {
          name: newActionInput,
          isUrgent: false
       }
-
+      // Unshift to actions array
       actions.push(newObjAction);
       elActionsList.append(elActionsItem);
    }
 
 
-
+   // Little UX feature 
    elNewActionInput.value = '';
    elNewActionInput.focus();
 
@@ -65,21 +67,30 @@ elActionsListForm.addEventListener('submit', function (evt) {
 
 
 
-
-
 /* ==============================
 EVENT OF CHECKING ACTIONS
 ============================== */
 
+// Get html elemets
 var elActionsForm = document.querySelector('.actions-check-list__form');
 var elActionsInput = elActionsForm.querySelector('.action-check-input');
 var elActionsResult = elActionsForm.querySelector('.action-check__result');
 
+
+// Listen to submit of elActionsForm
 elActionsForm.addEventListener('submit', function (evt) {
    evt.preventDefault();
 
+   // Get value of elActionsInput
    var actionsInput = elActionsInput.value.trim();
 
+   // Prevent an empty inputl
+   if (!actionsInput) {
+      alert(`Please, enter text!`);
+   }
+
+
+   // Check whether input is added or not
    var isActionsInputAdded = false;
 
    actions.forEach(function (action) {
@@ -89,16 +100,14 @@ elActionsForm.addEventListener('submit', function (evt) {
    });
 
 
-   console.log(isActionsInputAdded)
-   if (!actionsInput) {
-      alert(`Please, enter text!`);
-   }
-
+   // Release box if action is added
    if (isActionsInputAdded) {
       elActionsResult.classList.remove('d-none');
       elActionsResult.textContent = `Yes, you added ${actionsInput}`;
    } else {
       elActionsResult.classList.add('d-none');
+
+      // Little UX feature
       elNewActionInput.value = actionsInput;
       elActionsInput.value = '';
       elNewActionInput.focus();
